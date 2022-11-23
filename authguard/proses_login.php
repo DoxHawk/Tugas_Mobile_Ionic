068443226ff8c3d70d1bfd295e0b6827dfdd60de
DoxHawk <87254209+DoxHawk@users.noreply.github.com>
2022-11-23 14:16:41 +0700

    Ubah database

--- authguard/proses_login.php
+++ authguard/proses_login.php
@@ -5,12 +5,12 @@ $data = json_decode($input,true);
 $pesan = [];
 $username = trim($data['username']);
 $password = md5(trim($data['password']));
-$query = mysqli_query($koneksi,"select * from user where username='$username' and password='$password'");
+$query = mysqli_query($koneksi,"SELECT * FROM tbl_users WHERE user_name='$username' AND user_password='$password'");
 $jumlah = mysqli_num_rows($query);
 if ($jumlah != 0) {
 	$value = mysqli_fetch_object($query);
-	$pesan['username'] = $value->username;
-	$pesan['token'] = time().'_'.$value->password;
+	$pesan['username'] = $value->user_name;
+	$pesan['token'] = time().'_'.$value->user_password	;
 	$pesan['status_login']= 'berhasil';
 }else{
 	$pesan['status_login']= 'gagal';
