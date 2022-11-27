8e7a914740179974138ed77bf19b49b82fc23b54
EXOthicc <ahmd.habiballah@gmail.com>
2022-11-28 00:01:23 +0700

    user & admin hanya bisa mengakses pagenya sendiri

--- src/app/guards/admn.guard.ts
+++ src/app/guards/admn.guard.ts
@@ -2,26 +2,84 @@ import { AuthenticationService } from '../services/authentication.service';
 import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Preferences } from '@capacitor/preferences';
 import { filter, map, take } from 'rxjs/operators';
+
+const TYPE = 'tipe';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdmnGuard implements CanLoad {
+  public type: string ;
   constructor(private authService: AuthenticationService, private router: Router)
   {}
 
-  canLoad(): Observable<boolean>{
+  async getType(){
+    const ambilType = await Preferences.get({ key: TYPE});
+          if( ambilType && ambilType.value){
+            let typeuser = ambilType.value;
+            this.type = typeuser;
+            this.haveValue();
+            this.canLoad();
+          }
+          else{
+            
+          }
+
+  }
+  canLoad(){
+    if(this.type === undefined){
+      this.getType();
+      return true;
+    }
+    else{
+      const ambilType = Preferences.get({ key: TYPE});
+      if(this.type === '1'){ //1=admin
+        console.log("adalah user")
+        return this.authService.isAuthenticated.pipe(
+          filter((val) => val !== null),
+          take(1),
+          map((isAuthenticated) => {
+            if(isAuthenticated){
+                console.log("typeA", this.type);
+                return true;
+            }
+            else {
+              this.router.navigateByUrl('/');
+              console.log('anda harus login terlebih dahulu');
+              return false;
+            }
+          })
+        );
+      }
+      else{
+        console.log("bukanlah admin");
+        this.router.navigateByUrl('/');
+        return false;
+      }
+      return true;
+      
+    }
+    
+  }
+
+  haveValue(): Observable<boolean>{
     return this.authService.isAuthenticated.pipe(
       filter((val) => val !== null),
       take(1),
       map((isAuthenticated) => {
         if(isAuthenticated){
-          return true;
+          if( this.type == '1'){ //1=admin
+            return true;
+          }
+          else{
+            return false;
+          }
         }
         else {
           this.router.navigateByUrl('/');
-          console.log('hanya admin yang dapat mengakses page tersebut');
+          console.log('anda harus login terlebih dahulu');
           return false;
         }
       })
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,22 +2,81 @@ import { AuthenticationService } from '../services/authentication.service';
 import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Preferences } from '@capacitor/preferences';
 import { filter, map, take } from 'rxjs/operators';
+
+const TYPE = 'tipe';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
+  public type: string ;
   constructor(private authService: AuthenticationService, private router: Router)
   {}
 
-  canLoad(): Observable<boolean>{
+  async getType(){
+    const ambilType = await Preferences.get({ key: TYPE});
+          if( ambilType && ambilType.value){
+            let typeuser = ambilType.value;
+            this.type = typeuser;
+            this.haveValue();
+            this.canLoad();
+          }
+          else{
+            
+          }
+
+  }
+
+  canLoad(){
+    if(this.type === undefined){
+      this.getType();
+      return true;
+    }
+    else{
+      const ambilType = Preferences.get({ key: TYPE});
+      if(this.type === '0'){ //0=user
+        console.log("adalah user")
+        return this.authService.isAuthenticated.pipe(
+          filter((val) => val !== null),
+          take(1),
+          map((isAuthenticated) => {
+            if(isAuthenticated){
+                console.log("typeA", this.type);
+                return true;
+            }
+            else {
+              this.router.navigateByUrl('/');
+              console.log('anda harus login terlebih dahulu');
+              return false;
+            }
+          })
+        );
+      }
+      else{
+        console.log("bukanlah user");
+        this.router.navigateByUrl('/');
+        return false;
+      }
+      return true;
+      
+    }
+    
+  }
+
+  haveValue(): Observable<boolean>{
     return this.authService.isAuthenticated.pipe(
       filter((val) => val !== null),
       take(1),
       map((isAuthenticated) => {
         if(isAuthenticated){
-          return true;
+          if( this.type == '0'){ //0=user
+            return true;
+          }
+          else{
+            return false;
+          }
         }
         else {
           this.router.navigateByUrl('/');
